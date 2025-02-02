5edb00f72299dcb375e95290332135f81e4ce814
Bart <dirkjanapp@outlook.com>
2025-02-02 13:45:17 +0100

    cleanup

--- app.js
+++ app.js
@@ -39,15 +39,13 @@ function Addfav()
 	if(favs.indexOf(formattedComicDate) == -1)
 	{
 		favs.push(formattedComicDate);
-		//$(".favicon").css({"color": "red"}).removeClass('fa-heart-o').addClass('fa-heart');
 		document.getElementById("favheart").src="./heart.svg";
 		document.getElementById("showfavs").disabled = false;
 	}
 	else
 	{
 		favs.splice(favs.indexOf(formattedComicDate), 1);
-	//	$(".favicon").css({"color": "red"}).removeClass('fa-heart').addClass('fa-heart-o');
-		document.getElementById("favheart").src="heartborder.svg";
+		document.getElementById("favheart").src="./heartborder.svg";
 		if(favs.length === 0)
 		{
 			document.getElementById("showfavs").checked = false;
