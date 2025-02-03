3a21ad2f00b76e994278ca66b371855b8563da40
Bart <dirkjanapp@outlook.com>
2025-02-03 19:07:20 +0100

    serviceworker

--- app.js
+++ app.js
@@ -420,5 +420,40 @@ else
 	document.getElementById("settingsDIV").style.display = "none";
 }
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
+
+function showInstallPromotion() {
+  const installButton = document.createElement('button');
+  installButton.innerText = 'Install App';
+  installButton.style.position = 'fixed';
+  installButton.style.bottom = '10px';
+  installButton.style.right = '10px';
+  document.body.appendChild(installButton);
+
+  installButton.addEventListener('click', () => {
+	// Hide the app provided install promotion
+	installButton.style.display = 'none';
+	// Show the install prompt
+	deferredPrompt.prompt();
+	// Wait for the user to respond to the prompt
+	deferredPrompt.userChoice.then((choiceResult) => {
+	  if (choiceResult.outcome === 'accepted') {
+		console.log('User accepted the install prompt');
+	  } else {
+		console.log('User dismissed the install prompt');
+	  }
+	  deferredPrompt = null;
+	});
+  });
+}
 	
 	   
