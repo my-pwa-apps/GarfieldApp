798900a04af429745fab203f3b36a73bf301cd38
GarfieldApp <garfieldapp@outlook.com>
2025-11-07 21:56:58 +0100

    Update toolbar button styles and increment service worker version to v9

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v8';
+const VERSION = 'v9';
 const CACHE_NAME = `garfield-${VERSION}`;
 const RUNTIME_CACHE = `garfield-runtime-${VERSION}`;
 const IMAGE_CACHE = `garfield-images-${VERSION}`;
