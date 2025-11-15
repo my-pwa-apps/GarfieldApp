b9850c4f733724c1e899cbf13216ec2d78f6f7d4
GarfieldApp <garfieldapp@outlook.com>
2025-11-15 20:56:25 +0100

    Update version to v69 in service worker and remove unnecessary width and height properties in main.css

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v68';
+const VERSION = 'v69';
 const CACHE_NAME = `garfield-${VERSION}`;
 const RUNTIME_CACHE = `garfield-runtime-${VERSION}`;
 const IMAGE_CACHE = `garfield-images-${VERSION}`;
