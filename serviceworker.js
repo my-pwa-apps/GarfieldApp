b9be2ee2458af23cf90636a23469d478f0c781bb
GarfieldApp <garfieldapp@outlook.com>
2025-11-16 10:29:50 +0100

    Update service worker version to v75

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v74';
+const VERSION = 'v75';
 const CACHE_NAME = `garfield-${VERSION}`;
 const RUNTIME_CACHE = `garfield-runtime-${VERSION}`;
 const IMAGE_CACHE = `garfield-images-${VERSION}`;
