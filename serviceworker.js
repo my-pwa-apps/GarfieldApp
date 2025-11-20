04b6717b9af0772dde29bde3dee6453d11914936
GarfieldApp <garfieldapp@outlook.com>
2025-11-20 11:43:28 +0100

    Bump service worker version to v100

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v98';
+const VERSION = 'v100';
 const CACHE_NAME = `garfield-${VERSION}`;
 const RUNTIME_CACHE = `garfield-runtime-${VERSION}`;
 const IMAGE_CACHE = `garfield-images-${VERSION}`;
