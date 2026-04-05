de7a9e265c29e1b5cfbbbdffec0f7ab129862c0d
GarfieldApp <garfieldapp@outlook.com>
2026-04-05 14:09:23 +0200

    Add source map for favorites API worker and middleware integration

--- worker/favorites-api/.wrangler/tmp/dev-msrPyB/index.js
+++ worker/favorites-api/.wrangler/tmp/dev-msrPyB/index.js
@@ -192,7 +192,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-idQ35X/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-TJl8Id/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -224,7 +224,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-idQ35X/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-TJl8Id/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
