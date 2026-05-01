ce9294b9eca30d2d8be1e5171187d2292a49ffb8
GarfieldApp <garfieldapp@outlook.com>
2026-05-01 15:47:02 +0200

    fix: update version number to v1.12.86 and adjust logo styling for mobile responsiveness

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v1.12.85';
+const VERSION = 'v1.12.86';
 const CACHE_NAME = `garfield-${VERSION}`;
 const RUNTIME_CACHE = `garfield-runtime-${VERSION}`;
 const IMAGE_CACHE = `garfield-images-${VERSION}`;
